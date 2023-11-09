d49c665f3705600a47069fc6c9271ae4a1889704
Amanda kummel <135257189+amandakummeldeandrtadepero@users.noreply.github.com>
2023-11-09 10:34:12 -0300

    formasgeometricas.java

---
+++ aula 4/exercício02/formasgeometricas.java
@@ -0,0 +1,10 @@
+package geometrica;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class formaGeometrica {
+    public abstract double calcularArea();
+    public abstract String getNome();
+
+}
